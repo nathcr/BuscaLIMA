4dee7e575dc5eb7a359f42600b0fd33a23c9eb94
Mariana Chavez <141682590+Mariana-Chavez@users.noreply.github.com>
2023-11-22 15:40:09 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='BuscaLiMA',
+setup(name='buscaminas',
       version='0.1',
       description='creating a graphical minesweeper using python',
       url='https://github.com/emafriki/BuscaLIMA,
