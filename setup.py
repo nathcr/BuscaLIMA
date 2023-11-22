7ad95c8acdce0bcabdee094bdee07ae689bb1f16
Mariana Chavez <141682590+Mariana-Chavez@users.noreply.github.com>
2023-11-22 15:39:04 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='BuscaLiMA',
       author=['Alma Rocio Patiño Chavez', 'Maritza Nazareth Cruz Aviles', 'Mauritania Meneses Bote', 'Mariana Chavez Perales']
       author_email=['pa401981@uaeh.edu.mx', 'cr441643@uaeh.edu.mx','me340563@uaeh.edu.mx','ch400795@uaeh.edu.mx']
       license='MIT',
-      packages=['BuscaLiMA'],
+      packages=['buscaminas'],
       zip_safe=False)
